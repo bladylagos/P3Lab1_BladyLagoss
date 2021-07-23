bee90c9d7ddeb059ff82984c32c389ecba763e67
blady lagos <78231413+bladylagos@users.noreply.github.com>
2021-07-23 16:08:14 -0600

    Laboratorio Terminado

--- main.cpp
+++ main.cpp
@@ -1,51 +1,62 @@
 #include <iostream>
-
+#include <math.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
+
+int menu();
+void Sumatoria(int);
+void Operacion(int);
+
 int main(int argc, char** argv) {
-	int menu=0;
 	
-	while(menu!=3) {
-		
-		cout<<"\n* * * * * Escoja una Opcion * * * * *\n"<<endl<<"	  1. Ejercicio 1"<<endl<<"	  2. Ejercicio 2"<<endl<<"	  3. Salir!!!\n"<<endl;
-		
-		cin>>menu;
-		
-		if(menu==1) {
+	int opcion=0;
+
+	while((opcion = menu()) != 3){
 			
-			int num;
-			cout<<"\nIngrese numero: "<<endl;
-			
-			cin>> num;
-			
-			//decision
-			if(num<0) {
-				cout<<"Ingrese solo numeros positivos"<<endl;
+		if(opcion==1){
+				int num=0;
+				
+				while(num<1){
+					cout<< "ingrese un numero: ";
+					cin >> num;
+				}
+				Sumatoria(num);	
+				
+		}else {
+				
+			if(opcion == 2){
+				int x=0;
+				cout << "ingrese un valor x: ";
+				cin >>x;
+				Operacion(x);
 			}
-			//calculos y declaracion de variables
-            double suma = 0;
-            for (int n = 1; n <= num; n++) {
-                double signo = 1;
-                for (int i = 1; i <= n; i++) {
-                    signo = -signo;
-                }
-                double num = 3;
-                for (int i = 1; i <= 2 * n; i++) {
-                    num = num * n;
-                }
-                double denum = 1;
-                for (int i = 1; i <= 2 * n + 1; i++) {
-                    denum = denum * i;
-                }
-                //calculo de la sumatoria
-                suma = suma + signo * num / denum;
-            }
-			cout<<"La respuesta es: "<<suma<<"\n";
 		}
-		
-		if(menu==2){
-			//
-		}
-   } 
+	}
 	return 0;
 }
+	
+	int menu(){
+			
+		int opc=0;
+		cout<<"\n* * * * * Escoja una Opcion * * * * *"<<endl<<"	 1. Ejercicio 1"<<endl<<"	 2. Ejercicio 2"<<endl<<"	 3. Salirse"<<endl;
+		cout<<"\nIngrese que opcion desea: ";
+		cin >> opc;             
+		return opc;
+	}
+		
+	void Sumatoria(int n){
+			
+	double acum=0;
+		for(int i=1; i<=n; i++){
+			double num=2.0 *i -1.0 ;
+			double den= i * ( i + 1.0 );
+			acum+=num / den;
+		}
+		cout<<"La sumatoria del numero "<<n<<" es: "<<acum<<endl;
+	}
+		
+	void Operacion(int x){
+		double resul = 1.0 / (1.0 + exp(x)) ;
+		cout << "La operacion del numero "<< x <<" es: " << resul << endl;			
+	}
+
